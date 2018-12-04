0b0e3aee1592ace9f9a0a74529e3d49128064b85
StevenSeeger <sseeger1996@gmail.com>
2018-12-04 16:26:42 -0700

    Fixed to be less than 150

--- Load Data.R
+++ Load Data.R
@@ -6,18 +6,18 @@ AllData <- AllData[c(8,15)]
 library(dplyr)
 married <- AllData %>% filter(marital_status == "M")
 married <- married %>% mutate(detail_age = as.numeric(detail_age))
-married <- married %>% filter(detail_age >= 18)
+married <- married %>% filter(detail_age >= 18 & detail_age <= 150)
 
 single <- AllData %>% filter(marital_status == "S")
 single <- single %>% mutate(detail_age = as.numeric(detail_age))
-single <- single %>% filter(detail_age >= 18)
+single <- single %>% filter(detail_age >= 18 | detail_age <= 150)
 
 widow <- AllData %>% filter(marital_status == "W")
 widow <- widow %>% mutate(detail_age = as.numeric(detail_age))
-widow <- widow %>% filter(detail_age >= 18)
+widow <- widow %>% filter(detail_age >= 18 | detail_age <= 150)
 
 divorced <- AllData %>% filter(marital_status == "D")
 divorced <- divorced %>% mutate(detail_age = as.numeric(detail_age))
-divorced <- divorced %>% filter(detail_age >= 18)
+divorced <- divorced %>% filter(detail_age >= 18 | detail_age <= 150)
 
 save(AllData, single, married, file="data.RData")
