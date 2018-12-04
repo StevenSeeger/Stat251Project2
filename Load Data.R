09e93b628b70bc5edd2667ad6935ba4c1afc6a06
StevenSeeger <sseeger1996@gmail.com>
2018-12-04 16:32:11 -0700

    Fixed bugs

--- Load Data.R
+++ Load Data.R
@@ -12,14 +12,14 @@ married <- married %>% filter(detail_age >= 18 & detail_age <= 150)
 
 single <- AllData %>% filter(marital_status == "S")
 single <- single %>% mutate(detail_age = as.numeric(detail_age))
-single <- single %>% filter(detail_age >= 18 | detail_age <= 150)
+single <- single %>% filter(detail_age >= 18 & detail_age <= 150)
 
 widow <- AllData %>% filter(marital_status == "W")
 widow <- widow %>% mutate(detail_age = as.numeric(detail_age))
-widow <- widow %>% filter(detail_age >= 18 | detail_age <= 150)
+widow <- widow %>% filter(detail_age >= 18 & detail_age <= 150)
 
 divorced <- AllData %>% filter(marital_status == "D")
 divorced <- divorced %>% mutate(detail_age = as.numeric(detail_age))
-divorced <- divorced %>% filter(detail_age >= 18 | detail_age <= 150)
+divorced <- divorced %>% filter(detail_age >= 18 & detail_age <= 150)
 
 
