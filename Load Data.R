5fe5e67d45e01f052287d6237a4412aec2e8d67a
StevenSeeger <sseeger1996@gmail.com>
2018-12-04 16:23:11 -0700

    Subseting to older than 18

--- Load Data.R
+++ Load Data.R
@@ -3,21 +3,21 @@ library(readr)
 AllData <- read_csv("\2015_data.csv")
 AllData <- AllData[c(8,15)]
 
-married <- which(AllData$marital_status == "M")
-married <- AllData[married]
-
 library(dplyr)
 married <- AllData %>% filter(marital_status == "M")
 married <- married %>% mutate(detail_age = as.numeric(detail_age))
+married <- married %>% filter(detail_age >= 18)
 
 single <- AllData %>% filter(marital_status == "S")
 single <- single %>% mutate(detail_age = as.numeric(detail_age))
-single18 <- single %>% filter(detail_age >= 18)
+single <- single %>% filter(detail_age >= 18)
 
 widow <- AllData %>% filter(marital_status == "W")
 widow <- widow %>% mutate(detail_age = as.numeric(detail_age))
+widow <- widow %>% filter(detail_age >= 18)
 
 divorced <- AllData %>% filter(marital_status == "D")
 divorced <- divorced %>% mutate(detail_age = as.numeric(detail_age))
+divorced <- divorced %>% filter(detail_age >= 18)
 
 save(AllData, single, married, file="data.RData")
