6b4dee693e33d91a843eb4dcd17f5985fdf3ef46
StevenSeeger <sseeger1996@gmail.com>
2018-12-04 16:41:22 -0700

    Added Density Distribtuion and changed write up to say it was an uninformative prior

---
+++ Density Distribution Graphs.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+
+Sgraph <- ggplot(single, aes(x=detail_age)) + 
+  geom_histogram(aes(y=..density..), colour="black", fill="white", binwidth = 5)+
+  geom_density(alpha=.2, fill="#FF6666") + 
+  geom_vline(aes(xintercept=mean(detail_age)),
+             color="lightsteelblue3", linetype="dashed", size=1) +
+  geom_vline(aes(xintercept=median(detail_age)),
+             color="darkseagreen3", linetype="dashed", size=1) +
+  xlab("Fms") + 
+  ylab("Density") +
+  ggtitle("Single Age of Death Density Distribution")
+
+Mgraph <- ggplot(married, aes(x=detail_age)) + 
+  geom_histogram(aes(y=..density..), colour="black", fill="white", binwidth = 5)+
+  geom_density(alpha=.2, fill="#FF6666") + 
+  geom_vline(aes(xintercept=mean(detail_age)),
+             color="lightsteelblue3", linetype="dashed", size=1) +
+  geom_vline(aes(xintercept=median(detail_age)),
+             color="darkseagreen3", linetype="dashed", size=1) +
+  xlab("Fms") + 
+  ylab("Density") +
+  ggtitle("Married Age of Death Density Distribution")
