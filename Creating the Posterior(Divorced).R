8db5a2860b18a06592cebb92479819094334e5fe
StevenSeeger <sseeger1996@gmail.com>
2018-12-05 06:06:29 -0700

    Adding Divorced Posterior

---
+++ Creating the Posterior(Divorced).R
@@ -0,0 +1,42 @@
+library(MASS)
+library(invgamma)
+
+y<-divorced$detail_age
+n<-length(y)
+ybar<-mean(y)
+
+m<-0
+v<-10^2
+a<-0.01
+b<-0.01
+
+#Create vectors
+mu<-numeric()
+sig2<-numeric()
+
+nRep<-10000
+#The order of sig2 and mu doesn't matter--the starting value doesn't either
+sig2[1]<-var(y)
+mu[1]<-ybar #optional--you only need to start with one of the two
+#mu[1]<--3000
+
+for(j in 2:nRep) {
+  #update mu based on sigma^2
+  vstar<-1/(n/sig2[j-1]+1/v)
+  mstar<-vstar*(n*ybar/sig2[j-1]+m/v)
+  mu[j]<-rnorm(1,mstar,sqrt(vstar))
+  
+  #update sigma^2 based on updated mu
+  astar<-n/2+a
+  bstar<-sum((y-mu[j])^2)+b
+  sig2[j]<-rinvgamma(1,astar,rate=bstar)
+}
+
+divorcedMu <- mu
+divorcedSig2 <- sig2
+
+plot(divorcedMu, type='l')
+plot(divorcedSig2, type='l')
+
+mean(divorcedMu[-c(1:500)])
+sqrt(mean(divorcedSig2[-c(1:500)]))
